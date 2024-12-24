757452d46f18bda332b369b99ddb6f369ad12284
Nikita <enagtim@mail.ru>
2024-12-24 12:15:05 +0300

    add connection mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './room/room.module';
 import { ScheduleModule } from './schedule/schedule.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [RoomModule, ScheduleModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot('mongodb://admin:admin@localhost:27017/booking-api?authSource=admin'),
+		RoomModule,
+		ScheduleModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
